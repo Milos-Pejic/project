29ccf67ac2fd980f26b775029b03bcfa4a881d68
Milos Pejic <milosp@examroom.ai>
2024-05-06 13:26:45 +0200

    ++

---
+++ src/app/modules/organizations/components/organization-shell/organization-shell.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organization-shell',
+  templateUrl: './organization-shell.component.html',
+  styleUrl: './organization-shell.component.scss'
+})
+export class OrganizationShellComponent {
+
+}
